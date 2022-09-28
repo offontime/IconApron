a61265bdc72a721c549a0aefe2731e5144584fbb
Kimi Tian <yuke.tian@davincimotor.com>
2022-09-28 16:43:19 +0800

    update, vue2 component

---
+++ vue/package/IconApron/index.js
@@ -0,0 +1,8 @@
+import IconApron from './component'
+
+/* istanbul ignore next */
+IconApron.install = function (Vue) {
+  Vue.component(IconApron.name, IconApron)
+}
+
+export default IconApron
--- vue/src/main.js
+++ vue/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import IconApron from '../package/index.js'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(IconApron)
 
 new Vue({
   render: h => h(App)
